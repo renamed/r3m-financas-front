7f603eeaf01dfc817862a490395b6f904fb5770c
Renato Medeiros <renamedrj@gmail.com>
2025-08-10 15:20:06 -0300

    fix: production script

--- run_prod.ps1
+++ run_prod.ps1
@@ -1,11 +1,20 @@
 # check whether git branch is main
 if ((git rev-parse --abbrev-ref HEAD) -eq "main") {    
-    Write-Information "You are on the main branch. Proceeding with production build..."
+    Write-Host "You are on the main branch. Proceeding with production build..."
     npm run deploy
 }
 else {
-    Write-Warning "You are not on the main branch. We'll run the last production build instead."
+    Write-Host "You are not on the main branch. We'll run the last production build instead."
 }
 
+if ((npm list -g --depth=0 --json | ConvertFrom-Json).dependencies.'serve') {
+    Write-Host "'serve' já está instalado."
+} else {
+    Write-Host "Instalando 'serve'..."
+    npm install -g serve
+}
+
+
 # rodar o projeto usando http-server
-http-server dist\r3m-financas-front\browser -p 4201
+Set-Location 'C:\git\r3m-financas-front\dist\r3m-financas-front\browser'
+serve -s . -l 4201
